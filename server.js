b2d0caacb64fd11f2c29e29c57da26dafe30cf82
chandra8120 <chandrasekhar8120@gmail.com>
2024-04-14 17:23:23 +0530

    chandra link new mongo database

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ import connectToMongoDB from "./conn.js";
 import routes from "./routes/routes.js";
 import dotenv from "dotenv";
 import cors from 'cors';
-// import multer from 'multer'
-
+// import multer from 'multer
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
